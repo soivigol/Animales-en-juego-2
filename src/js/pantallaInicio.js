83786a9a392e9bc262725f656b1720a27c0676ee
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-23 07:09:01 +0100

    js

--- src/js/pantallaInicio.js
+++ src/js/pantallaInicio.js
@@ -4,8 +4,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady () {
 	// body...
-$('button').on('touchend', function(){
-window.plugins.deviceFeedback.acoustic()
+$('a').on('touchend', function(){
+window.plugins.deviceFeedback.acoustic();
 });
 
 puntuacion=localStorage.getItem("puntuacion");
