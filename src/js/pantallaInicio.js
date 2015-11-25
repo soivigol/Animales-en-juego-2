ffbc125fbf68b28c9cf76d433e43814aa6393632
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-25 12:17:33 +0100

    bd add2

--- src/js/pantallaInicio.js
+++ src/js/pantallaInicio.js
@@ -19,9 +19,10 @@ tx.executeSql('CREATE TABLE IF NOT EXISTS animales(id integer primary key, sonid
 var sonido=['abeja.mp3','burro.mp3','caballo.mp3','cabra.mp3','canario.mp3','cerdo.mp3','cuervo.mp3','elefante.mp3','gallo.mp3','gato.mp3','grillo.mp3','leon.mp3','lobo.mp3','mono.mp3','oso.mp3','oveja.mp3','pato.mp3','pavo.mp3','perro.mp3','rana.mp3','vaca.mp3'];
 var imagen=['Abeja.png','Burro.png','Caballo.png','Cabra.png','Canario.png','Cerdo.png','Cuervo.png','Elefante.png','Gallo.png','Gato.png','Grillo.png','Leon.png','Lobo.png','Mono.png','Oso.png','Oveja.png','Pato.png','Pavo.png','Perro.png','Rana.png','Vaca.png'];
 var nombre=['Abeja','Burro','Caballo','Cabra','Canario','Cerdo','Cuervo','Elefante','Gallo','Gato','Grillo','Leon','Lobo','Mono','Oso','Oveja','Pato','Pavo','Perro','Rana','Vaca'];
-for (var i = 0; i < Things.length; i++) {
+for (var i = 0; i < 21; i++) {
 tx.executeSql("INSERT INTO animales(sonido,imagen,nombre) VALUES (?,?,?)", [sonido[i],imagen[i],nombre[i]]);
 }
+localStorage.setItem('primeraVez','11');
 });
 }
 $('#contPuntuacion').html('Puntuación='+puntuacion);
---
+++ src/js/sonidos.js
@@ -0,0 +1,13 @@
+var puntuacion=0;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady () {
+var db = window.sqlitePlugin.openDatabase({name: "animales.db", androidLockWorkaround: 1});
+db.transaction(function(tx) {
+tx.executeSql("SELECT * FROM animales",[],function(tx,res){
+for(var i = 0; i < res.rows.length; i++)
+{
+$('body').append('<p>'+res.rows.item(i).sonido+'-'+res.rows.item(i).imagen+'-'+res.rows.item(i).nombre+'</p>');
+});
+}
