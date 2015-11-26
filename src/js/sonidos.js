57d7c774e7b35b0d0c9e777fa912f6b1119d6ccf
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-26 17:22:16 +0100

    15

--- src/js/sonidos.js
+++ src/js/sonidos.js
@@ -21,7 +21,7 @@ function iniciar(){
 $('#pagBienvenida').css('display','none');
 $('#pagPpal').css('display','block');
 $('#contPuntuacion').html('<p>Puntuación='+puntuacion+'</p>');
-colocarAnimales():
+colocarAnimales();
 }
 
 function aleatorio(a){
@@ -30,5 +30,24 @@ return Math.round(Math.random()*a);
 
 function colocarAnimales(){
 alert('hola')
+animal1=aleatorio(20);
+animal2=aleatorio(20);
+animal3=aleatorio(20);
+var db = window.sqlitePlugin.openDatabase({name: "animales.db", androidLockWorkaround: 1});
+db.transaction(function(tx) {
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal1+"'",[],function(tx,res){
+rutaAnimal1=res.rows.item(0).imagen;
+});
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal2+"'",[],function(tx,res){
+rutaAnimal2=res.rows.item(0).imagen;
+});
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal3+"'",[],function(tx,res){
+rutaAnimal3=res.rows.item(0).imagen;
+});
+});
+
+window.setTimeout(function(){
+$('#contAnimales').html('<div class="arriba3"><div>'+rutaAnimal1+'</div><div>'+rutaAnimal2+'</div></div><div class="abajo3">'+rutaAnimal3+'</div>');
+},70);
 
 }
