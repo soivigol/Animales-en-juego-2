1213494fcd0580efddd93b83931965f927bea4ba
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-26 08:40:54 +0100

    sonidosAct

--- src/js/sonidos.js
+++ src/js/sonidos.js
@@ -1,14 +1,27 @@
 var puntuacion=0;
+var db;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady () {
-var db = window.sqlitePlugin.openDatabase({name: "animales.db", androidLockWorkaround: 1});
+puntuacion=localStorage.getItem("puntuacion");
+if (puntuacion==null) {
+puntuacion=0;
+};
+$('#contPuntuacion').html('<p>Puntuación='+puntuacio+'</p>');
+window.setTimeout(iniciar,2000);
+db = window.sqlitePlugin.openDatabase({name: "animales.db", androidLockWorkaround: 1});
+
+}
+
+function iniciar(){
+$('#pagBienvenida').css('display','none');
+$('#pagPpal').css('display','block');
 db.transaction(function(tx) {
 tx.executeSql("SELECT * FROM animales",[],function(tx,res){
 for(var i = 0; i < res.rows.length; i++)
 {
-$('body').append('<p>'+res.rows.item(i).sonido+'-'+res.rows.item(i).imagen+'-'+res.rows.item(i).nombre+'</p>');
+$('#pagPpal').append('<p>'+res.rows.item(i).sonido+'-'+res.rows.item(i).imagen+'-'+res.rows.item(i).nombre+'</p>');
 }
 });
 });
