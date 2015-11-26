3d5b4cf096eeb6dde8c0ed7670e78dd4976a9c31
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-26 12:07:55 +0100

    7

--- src/js/sonidos.js
+++ src/js/sonidos.js
@@ -1,5 +1,11 @@
 var puntuacion=0;
 var db;
+var animal1=0;
+var animal2=0;
+var animal3=0;
+var rutaAnimal1;
+var rutaAnimal2;
+var rutaAnimal3;
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
@@ -16,19 +22,33 @@ window.setTimeout(iniciar,2000);
 function iniciar(){
 $('#pagBienvenida').css('display','none');
 $('#pagPpal').css('display','block');
-var animal1=aleatorio(20);
-alert(animal1);
-$('#contPuntuacion').html('<p>Puntuación='+puntuacio+'</p>');
-db.transaction(function(tx) {
-tx.executeSql("SELECT * FROM animales",[],function(tx,res){
-for(var i = 0; i < res.rows.length; i++)
-{
-$('#pagPpal').append('<p>'+res.rows.item(i).sonido+'-'+res.rows.item(i).imagen+'-'+res.rows.item(i).nombre+'</p>');
-}
-});
-});
+$('#contPuntuacion').html('<p>Puntuación='+puntuacion+'</p>');
+colocarAnimales():
 }
 
 function aleatorio(a){
 return Math.round(Math.random()*a);
+}
+
+function colocarAnimales(){
+$('#contAnimales').html('');
+animal1=aleatorio(20);
+animal2=aleatorio(20);
+animal3=aleatorio(20);
+db.transaction(function(tx) {
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal1+"'",[],function(tx,res){
+rutaAnimal1=res.rows.item(0).imagen;
+});
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal2+"'",[],function(tx,res){
+rutaAnimal2=res.rows.item(0).imagen;
+});
+tx.executeSql("SELECT * FROM animales WHERE id='"+animal3+"'",[],function(tx,res){
+rutaAnimal3=res.rows.item(0).imagen;
+});
+});
+
+window.setTimeout(function(){
+$('#contAnimales').html('<div class="arriba3"><div>'+rutaAnimal1+'</div><div>'+rutaAnimal2+'</div></div><div class="abajo3">'+rutaAnimal3+'</div>');
+},70);
+
 }
