5a2e1fae81891970bb4a8ca29e8c6f94e08a0791
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-11-28 13:39:56 +0100

    66

---
+++ src/js/numeros.js
@@ -0,0 +1,22 @@
+var puntuacion=0;
+var animal=new Array();
+var rutaanimal= new Array();
+var animalSeleccionado=0;
+var nombreAnimal= new Array();
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady () {
+puntuacion=localStorage.getItem("puntuacion");
+if (puntuacion==null) {
+puntuacion=0;
+}
+window.setTimeout(iniciar,500);
+}
+
+function iniciar(){
+$('#pagBienvenida').css('display','none');
+$('#pagPpal').css('display','block');
+$('#contPuntuacion').html('<p>Puntuación='+puntuacion+'</p>');
+colocarAnimales();
+}
