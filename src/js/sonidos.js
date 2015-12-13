0cdaf2aba1a69f84a6d3fdb12e6ac3d70c0fd447
soivigol <soivigol.desarrollo.webs@gmail.com>
2015-12-13 14:41:33 +0100

    13

--- src/js/sonidos.js
+++ src/js/sonidos.js
@@ -13,14 +13,10 @@ document.onselectstart = function() {return false;}
 window.addEventListener("orientationchange", function() {
 window.setTimeout(colocarAltura,250);
 }, false);
-alert(device.version)
-audio=document.createElement('audio');
-audio.src='audio/tituloSonidos.mp3';
-audio.play();
-if(audio==null){
+
 var media = new Media('file:///android_asset/www/audio/tituloSonidos.mp3');
 media.play();
-}
+
 
 puntuacion=Number(localStorage.getItem("puntuacion"));
 if (puntuacion==null) {
